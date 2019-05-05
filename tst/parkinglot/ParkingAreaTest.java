dad28b44e23acb40fbe4b0941f15896596a086ed
Sunil Tomar <tomarji852@gmail.com>
2019-05-05 15:14:38 +0530

    Modified parking with new changes

--- tst/parkinglot/ParkingAreaTest.java
+++ tst/parkinglot/ParkingAreaTest.java
@@ -5,6 +5,8 @@ package parkinglot;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -52,7 +54,7 @@ class ParkingAreaTest {
 	}
 	
 	@Test
-	void testExitCarForGivenSlotNumberAndCarIsAvailableThenSlotThenReturnTrue() {
+	void testExitCarForGivenSlotNumberAndCarIsAvailableSlotThenReturnTrue() {
 		//Given: Total slot as 1 and slot number for car exit
 		int totalSlots = 1;
 		ParkingArea parkingArea = new ParkingArea(totalSlots); 
@@ -67,7 +69,7 @@ class ParkingAreaTest {
 	}
 	
 	@Test
-	void testExitCarForGivenSlotNumberAndSlotIsAreadyEmptyThenSlotThenReturnFalse() {
+	void testExitCarForGivenSlotNumberAndSlotIsAreadyEmptySlotThenReturnFalse() {
 		//Given: Total slot as 1 and slot number for car exit
 		//And : slot is already empty 
 		int totalSlots = 1;
@@ -80,5 +82,112 @@ class ParkingAreaTest {
 		//Then: Verify isExit status as false
 		assertEquals(false, isExit);
 	}
-
+	
+	@Test
+	void testGetSlotNumberByColorForGivenColorThenReturnListOfSlotNumbersWith2Entries() {
+		//Given: 3 car are parked and two of them are of white color 
+		int totalSlots = 3;
+		String color = "White";
+		ParkingArea parkingArea = new ParkingArea(totalSlots); 
+		parkingArea.parkCar("KA-012-12", "White"); // car 1
+		parkingArea.parkCar("KA-016-15", "Black"); // car 2
+		parkingArea.parkCar("KA-014-16", "White"); // car 3
+		
+		//When: Method getSlotNumberByColor is executed
+		ArrayList<Integer> slotNumbers = parkingArea.getSlotNumberByColor(color);
+		
+		//Then: Verify list has slot number as 1, 3
+		assertEquals(1,slotNumbers.get(0));
+		assertEquals(3,slotNumbers.get(1));
+	}
+	
+	@Test
+	void testGetSlotNumberByColorForGivenColorAndNoCarIsPresentInThenSlotForThatColorThenReturnZeroSlot() {
+		//Given: 3 car are parked and none of them are of white color 
+		int totalSlots = 3;
+		String color = "White";
+		ParkingArea parkingArea = new ParkingArea(totalSlots); 
+		parkingArea.parkCar("KA-012-12", "Blue"); // car 1
+		parkingArea.parkCar("KA-016-15", "Black"); // car 2
+		parkingArea.parkCar("KA-014-16", "Red"); // car 3
+		
+		//When: Method getSlotNumberByColor is executed
+		ArrayList<Integer> slotNumbers = parkingArea.getSlotNumberByColor(color);
+		
+		//Then: Verify list has no slots
+		assertEquals(true,slotNumbers.isEmpty());		
+	}
+	
+	@Test
+	void testgetSlotNumberByRegistrationForGivenRegistrationThenReturnListOfSlotNumbersWith1Entries() {
+		//Given: 3 car are parked and there is one car with given registration number
+		int totalSlots = 3;
+		String registration = "KA-012-12";
+		ParkingArea parkingArea = new ParkingArea(totalSlots); 
+		parkingArea.parkCar("KA-012-12", "White"); // car 1
+		parkingArea.parkCar("KA-016-15", "Black"); // car 2
+		parkingArea.parkCar("KA-014-16", "White"); // car 3
+		
+		//When: MethodgetSlotNumberByRegistration is executed
+		ArrayList<Integer> slotNumbers = parkingArea.getSlotNumberByRegistration(registration);
+		
+		//Then: Verify list has slot number as 1
+		assertEquals(1,slotNumbers.get(0));
+	}
+	
+	@Test
+	void testGetSlotNumberByRegistrationForGivenRegistrationThenReturnListOfSlotNumbersWith0Entries() {
+		//Given: 3 car are parked and there is no car with given registration number
+		int totalSlots = 3;
+		String registrateion = "MA-012-12";
+		ParkingArea parkingArea = new ParkingArea(totalSlots); 
+		parkingArea.parkCar("KA-012-12", "White"); // car 1
+		parkingArea.parkCar("KA-016-15", "Black"); // car 2
+		parkingArea.parkCar("KA-014-16", "White"); // car 3
+		
+		//When: Method getSlotNumberByRegistration is executed
+		ArrayList<Integer> slotNumbers = parkingArea.getSlotNumberByRegistration(registrateion);
+		
+		//Then: Verify empty list 
+		assertEquals(true,slotNumbers.isEmpty());
+	}
+	
+	@Test
+	void testGetRegistrationNumberByColorForGivenColorThenReturnListOfSlotNumbersWith2Entries() {
+		//Given: 3 car are parked and there are two cars of given color
+		int totalSlots = 3;
+		String color = "White";
+		ParkingArea parkingArea = new ParkingArea(totalSlots); 
+		parkingArea.parkCar("KA-012-12", "White"); // car 1
+		parkingArea.parkCar("KA-016-15", "Black"); // car 2
+		parkingArea.parkCar("KA-014-16", "White"); // car 3
+		
+		//When: Method getRegistrationNumber is executed
+		ArrayList<String> regNumbers = parkingArea.getRegistrationNumberByColor(color);	
+		
+		//Then: Verify list has two reg numbers "KA-014-16", "KA-012-12"
+		assertEquals("KA-012-12",regNumbers.get(0));
+		assertEquals("KA-014-16",regNumbers.get(1));
+	}
+	
+	@Test
+	void testGetRegistrationNumberByColorForGivenColorThenReturnListOfSlotNumbersWith0Entries() {
+		//Given: 3 car are parked and there are no cars of given color
+		int totalSlots = 3;
+		String color = "Red";
+		ParkingArea parkingArea = new ParkingArea(totalSlots); 
+		parkingArea.parkCar("KA-012-12", "White"); // car 1
+		parkingArea.parkCar("KA-016-15", "Black"); // car 2
+		parkingArea.parkCar("KA-014-16", "White"); // car 3
+		
+		//When: Method getRegistrationNumber is executed
+		ArrayList<String> regNumbers = parkingArea.getRegistrationNumberByColor(color);	
+		
+		//Then: Verify empty list 
+		assertEquals(true,regNumbers.isEmpty());
+	}
 }
+
+
+
+
